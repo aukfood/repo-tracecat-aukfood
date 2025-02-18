a0d2d6c14fb72a33390b924a022b9104b7c8cbb2
bouddha-fr <bouddha.whitehat@gmail.com>
2025-02-18 10:46:32 +0100

    enhancement extract ipv4 ipv6

--- custom_actions/ip_addresss.py
+++ custom_actions/ip_addresss.py
@@ -7,7 +7,7 @@ from pydantic import Field
 
 from tracecat_registry import registry
 
-IPV4_REGEX = r"\b(?:(?:25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\b"
+IPV4_REGEX = r"\b(?:25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.(?:25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.(?:25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.(?:25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\b"
 IPV6_REGEX = r"\b(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,7}:|(?:[0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,5}(?::[0-9a-fA-F]{1,4}){1,2}|(?:[0-9a-fA-F]{1,4}:){1,4}(?::[0-9a-fA-F]{1,4}){1,3}|(?:[0-9a-fA-F]{1,4}:){1,3}(?::[0-9a-fA-F]{1,4}){1,4}|(?:[0-9a-fA-F]{1,4}:){1,2}(?::[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:(?:(?::[0-9a-fA-F]{1,4}){1,6})|:(?:(?::[0-9a-fA-F]{1,4}){1,7}|:)\b"
 
 
@@ -29,15 +29,15 @@ def extract_ip_addresses(
     if isinstance(texts, str):
         texts = [texts]
 
-    ip_addresses = itertools.chain.from_iterable(
+    ip_candidates = itertools.chain.from_iterable(
         re.findall(IPV4_REGEX, text) + re.findall(IPV6_REGEX, text, re.IGNORECASE) for text in texts
     )
 
     # Validate IP addresses
     valid_ips = set()
-    for ip in ip_addresses:
+    for ip in ip_candidates:
         try:
-            ip_obj = ipaddress.ip_address(ip)
+            ip_obj = ipaddress.ip_address(ip.strip())
             if ip_version is None or ip_obj.version == ip_version:
                 valid_ips.add(str(ip_obj))
         except ValueError:
