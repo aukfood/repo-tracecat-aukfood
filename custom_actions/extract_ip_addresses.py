c3fa96736ed55e7f8fc7fe7c9cd036673e4de54b
bouddha-fr <bouddha.whitehat@gmail.com>
2025-02-24 10:38:04 +0100

    feat: library extract_ip_address

---
+++ custom_actions/extract_ip_addresses.py
@@ -0,0 +1,50 @@
+import ipaddress
+import itertools
+import re
+
+# Regex pour IPv4
+IPV4_REGEX = r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b"
+
+# Regex optimisé pour IPv6
+IPV6_REGEX = r"\b(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|(?=:)(?:(?::(?:[0-9a-fA-F]{1,4})){1,7}|:)|(?:[0-9a-fA-F]{1,4}:){1,7}:|(?:[0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,5}(?::[0-9a-fA-F]{1,4}){1,2}|(?:[0-9a-fA-F]{1,4}:){1,4}(?::[0-9a-fA-F]{1,4}){1,3}|(?:[0-9a-fA-F]{1,4}:){1,3}(?::[0-9a-fA-F]{1,4}){1,4}|(?:[0-9a-fA-F]{1,4}:){1,2}(?::[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:(?:(?::[0-9a-fA-F]{1,4}){1,6}))\b"
+
+def extract_ipv4_addresses(texts):
+    """Extrait les adresses IPv4 uniques à partir d'une liste de textes."""
+    if isinstance(texts, str):
+        texts = [texts]
+
+    ip_addresses = itertools.chain.from_iterable(
+        re.findall(IPV4_REGEX, text) for text in texts
+    )
+
+    valid_ips = set()
+    for ip in ip_addresses:
+        try:
+            ip_obj = ipaddress.ip_address(ip)
+            if ip_obj.version == 4:
+                valid_ips.add(str(ip_obj))
+        except ValueError:
+            continue  # Ignorer les IP invalides
+
+    return list(valid_ips)
+
+
+def extract_ipv6_addresses(texts):
+    """Extrait les adresses IPv6 uniques à partir d'une liste de textes."""
+    if isinstance(texts, str):
+        texts = [texts]
+
+    ip_addresses = itertools.chain.from_iterable(
+        re.findall(IPV6_REGEX, text) for text in texts
+    )
+
+    valid_ips = set()
+    for ip in ip_addresses:
+        try:
+            ip_obj = ipaddress.ip_address(ip)
+            if ip_obj.version == 6:
+                valid_ips.add(str(ip_obj))
+        except ValueError:
+            continue  # Ignorer les IP invalides
+
+    return list(valid_ips)
